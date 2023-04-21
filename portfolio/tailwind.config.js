a3de09fe60a0a7e691c7d80d30d12af0803d0ea5
DevonGifford <DevonGifford@outlook.com>
2023-04-21 23:20:51 +0200

    boilerplate typescript nextJS app - with some googlefonts and basic styling

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    maxWidth: {
+      container: "1440px",
+      contentContainer: "1140px",
+      containerSmall: "1024px",
+      containerxs: "768px",
+    },
+    extend: {
+      screens: {  //l refers to landscape orientation
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
+      fontFamily : {
+        bodyFont: ['Montserrat', 'sans-serif'],
+        titleFont: ['Inter', 'sans-serif']
+      },
+      boxShadow : {
+        navbarShadow: "0 10px 30px -10px rgba(2,12,27,0.7)",
+      },
+      colors: {
+        bodyColor: "#0A192F",
+        textGreen: "#64ffda",
+        textLight: "#ccd6f6",
+        textDark: "#8892b0",
+        hovercolor: "rgba(100, 255, 218, 0.1)", 
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
