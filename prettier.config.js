a0bf5aae642fc6d5f363efceb8e5ec3733fdb9c5
Devon Gifford <devongifford@outlook.com>
2024-11-29 20:31:45 +0100

    chore: upgrade to Prettier v3 and add Tailwind plugin

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
